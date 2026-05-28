40569aafdc27ce2b618e20c30c668547a763c2bc
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-05-28 17:31:08 +0530

    Solved and tested problem 3 factorial

---
+++ day1/day1problem3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int i,num,factorial=1;
+
+    printf("Emter any number:");
+    scanf("%d",&num);
+
+    for(int i=1;i<=num;i++)
+    {
+        factorial=factorial*i;
+    }
+
+    printf("The factorial of %d is:%d",num,factorial);
+
+    return 0;
+}
