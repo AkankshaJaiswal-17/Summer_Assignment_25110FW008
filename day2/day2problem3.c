aff7804cbbf9799fed6d7e53e08e5e1cd41159fb
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-05-29 12:02:16 +0530

    Added day2problem3.c to the assignment

--- day2/day2problem3.c
+++ day2/day2problem3.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
 
+int main()
+{
+
+    int num,rem,product=1,temp_no;
+
+    printf("Enter any nummber:");
+    scanf("%d",&num);
+
+    temp_no=num;
+
+    while(temp_no>0)
+    {
+        rem=temp_no%10;
+        product=product*rem;
+        temp_no=temp_no/10;
+    }
+
+    printf("Product of digits of %d is %d",num,product);
+
+    return 0;
+}
