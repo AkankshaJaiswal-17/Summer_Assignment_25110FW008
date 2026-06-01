6d5f5f4b5c5d6e656d1067b1cf2236026a85444c
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-06-01 18:44:21 +0530

    Added day5problem3.c to the assignment

---
+++ day5/day5problem2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() 
+{
+
+    int num, temp, rem, fact, sum=0;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (num > 0) {
+        rem = temp % 10;
+
+        int fact = 1;
+        for (int i = 1; i <= rem; i++) {
+            fact=fact*i;
+        }
+
+        sum = sum + fact;
+
+        temp = temp / 10;
+    }
+
+    if (sum == num) {
+        printf("%d is a STRONG number.\n", num);
+    } else {
+        printf("%d is NOT a STRONG number.\n", num);
+    }
+
+    return 0;
+}
