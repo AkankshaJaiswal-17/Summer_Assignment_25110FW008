995d3fd36f0190d71de231b6b90f1a7b73dab6f9
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-06-04 09:44:05 +0530

    Added repeated_number_pattern.cpp to the assignment

---
+++ day8/repeated_number_pattern.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern( int n)
+{
+    
+
+    for(int i= 0; i<n ; i++)
+    {
+        for( int j=0 ; j<=i ; j++)
+        {
+            cout << i+1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+ 
+int num;
+
+cout << "Enter any number :" ;
+cin >> num;
+pattern(num);
+
+    return 0;
+}
