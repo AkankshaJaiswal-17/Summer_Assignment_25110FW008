d9b39dd39c0e0ae524d4a8ae4b9e1147d4f0e8be
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-06-05 12:41:50 +0530

    Added hollow_square.cpp to the assignment

---
+++ day9/hollow_square.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern( int n)
+{
+    
+
+    for(int i= 0; i<n ; i++)
+    {
+        for( int j=0 ; j<n ; j++)
+        {
+           if(i==0||i==n-1||j==0||j==n-1)
+           {
+            cout << "* " ;
+               
+           }
+            
+            else
+            {
+                cout << "  " ;
+                
+            }           
+        }
+        
+        cout << endl;
+    }
+}
+
+int main(){
+ 
+int num;
+
+cout << "Enter any number :" ;
+cin >> num;
+pattern(num);
+
+    return 0;
+}
