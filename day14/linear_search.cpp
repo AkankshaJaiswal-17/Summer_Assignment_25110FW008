fad8561c5e68598d53f457b5272e70a5710be76b
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-06-10 10:23:02 +0530

    Added linear_search.cpp to the assignment

---
+++ day14/linear_search.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+
+int n;
+cout << "Enter no of elemnts: \n";
+cin >> n;
+
+int arr[n];
+
+cout << "Enter " << n << " elements:\n" ;
+for( int i=0 ; i<n ; i++){
+    cin >> arr[i] ;
+}
+
+int num ,loc;
+cout << "Enter element to search:\n";
+cin >> num ;
+
+for(int i=0 ; i<n ; i++){
+  if (  arr[i] == num ){
+    loc = i + 1 ;
+    break;}
+}
+
+cout << num << " is found at " << loc << " position.";
+
+return 0 ;
+}
