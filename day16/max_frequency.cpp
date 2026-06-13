1993bb4fee5e35973b3b2308532c5d4bef27c250
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-06-13 15:15:02 +0530

    Added max_frequency.cpp to the assignment

---
+++ day16/max_frequency.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+
+int n;
+cout << "Enter no of elemnts: \n";
+cin >> n;
+
+int arr[n];
+
+cout << "Enter " << n << " elements:\n" ;
+for( int i=0 ; i<n ; i++){
+    cin >> arr[i] ;
+}
+
+int maxFreq = 0;
+int maxElement;
+
+for(int i = 0; i < n; i++)
+{
+    int count = 0;
+
+    for(int j = 0; j < n; j++)
+    {
+        if(arr[i] == arr[j])
+        {
+            count++;
+        }
+    }
+
+    if(count > maxFreq)
+    {
+        maxFreq = count;
+        maxElement = arr[i];
+    }
+}
+
+cout << "Maximum frequency element is: "
+     << maxElement << endl;
+
+cout << "Frequency is: "
+     << maxFreq;
+return 0 ;
+}
