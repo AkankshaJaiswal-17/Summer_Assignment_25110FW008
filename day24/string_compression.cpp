5f385611b5be12d243b4818b041f23054cf61428
AkankshaJaiwal-17 <akankshagallery8@gmail.com>
2026-06-20 12:34:36 +0530

    Added string_compression.cpp to the assignment

---
+++ day24/string_compression.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+
+    char str[100];
+
+    cout << "Enter a string:\n";
+cin >> str;
+
+for(int i=0; str[i]!='\0'; i++)
+{
+    int count = 0;
+
+    for(int j=0; str[j]!='\0'; j++)
+    {
+        if(str[i] == str[j])
+        {
+            count++;
+        }
+    }
+
+    bool printed = false;
+
+    for(int k=0; k<i; k++)
+    {
+        if(str[k] == str[i])
+        {
+            printed = true;
+            break;
+        }
+    }
+
+    if(!printed)
+    {
+        cout << str[i] << count ;
+    }
+}
+    
+return 0 ;
+}
